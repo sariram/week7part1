0aa2ef7820d971465e95ea21cf650d7a354411e3
SariRam <dh_i6anzh@sariram.me>
2020-12-08 13:33:02 -0800

    Art Gallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-jQuery(function() {
+jQuery(function() {let my_switch = false;$('#b1').click(function () {if ( my_switch == false ) {my_switch = true;$('p').hide();$('#slide').show();} else {my_switch = false;}});});
 
-	$('#my_button').click(function () {
-	});
+jQuery(document).ready(function($){
+	$('#button1').on({'click': function(){$('#hi').attr('src','images/picasso.jpg');}});
+	$('#button2').on({'click': function(){$('#hi').attr('src','images/cezanne.png');}});
+	$('#button3').on({'click': function(){$('#hi').attr('src','images/benode.jpg');}});
+	$('#button4').on({'click': function(){$('#hi').attr('src','images/chagall.jpg');}});
+	$('#button5').on({'click': function(){$('#hi').attr('src','images/keefe.png');}});});
 
-});
+function refreshPage(){
+	window.location.reload();}
